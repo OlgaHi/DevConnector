1bae54843ff5497dc5eccfd8d9b5f775d15b7248
OlgaHi <46238682+OlgaHi@users.noreply.github.com>
2019-03-14 17:04:41 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const app = express();
+
+const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
+// Body parser middleware
+//app.use(bodyparser.urlencoded({ extended: false}));
+//app.use(bodyparser.json());
+
+//Connect to db
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDb Connected'))
+  .catch(err => console.log(err));
+
+//Let's write our first route
+app.get('/', (req, res) => res.send('Hello!'));
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port} `));
